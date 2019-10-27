2d0039f6ea9dac29aafc980b98bdca0a5c9bc444
kadjiah <kadijahwilson@pursuit.org>
2019-10-27 14:44:08 -0400

    getting deck_id and appending card image to startGame div

--- 21.js
+++ 21.js
@@ -4,24 +4,47 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const startButton = () => {
     const startButton = document.querySelector("#startButton")
-    startButton.addEventListener('click', getPlayerCards)
+    startButton.addEventListener('click', getCards)
 
-    // const startGame = document.querySelector("#gameButtons")
-    // startGame.addEventListener('click', startGame)
 }
 
-const getPlayerCards = async() => {
-    let url = "https://cors-anywhere.herokuapp.com/https://deckofcardsapi.com/"
+const getCards = async() => {
+    let url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
     try{
         let response = await axios.get(url)
-        console.log(response)
-        startGame(url)
+        // console.log(response)
+              startGame(response)
     }catch(err){
         console.log(err)
     }
 }
 
-const startGame = () => {
+const startGame = async(response) => {
+deckID = response.data.deck_id
+// console.log(deckID)
+let button = document.querySelector("#startButton")
+let startButtonDIV = document.querySelector("#startGame")
+startButtonDIV.removeChild(button)
 
+let deckidURL = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`
+ //  console.log(deckidURL)
+let res = await axios.get(deckidURL)
+// console.log(res.data.cards)
+getPlayerCards(res)
+}
+
+const getPlayerCards = (res) => {
+    let  startGameDIV = document.querySelector("#startGame")
+    let cardsDealt = res.data.cards
+    // console.log(cardsDealt)
+
+ for(let i = 0; i < cardsDealt.length; i++){
+    let playerCards = cardsDealt[i].image
+     let img = document.createElement('img')
+     img.src = playerCards
+     startGameDIV.append(img)
+    // console.log(playerCards)
+    img.append(playerCards)
+ }
 
 }
