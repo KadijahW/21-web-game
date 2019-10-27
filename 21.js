1ead4842d05c2d3764cb6a21c070d0d31d609c95
kadjiah <kadijahwilson@pursuit.org>
2019-10-27 15:47:43 -0400

    adding functionality to hit button

--- 21.js
+++ 21.js
@@ -35,16 +35,55 @@ getPlayerCards(res)
 
 const getPlayerCards = (res) => {
     let  startGameDIV = document.querySelector("#startGame")
+    let gameButtonDiv = document.querySelector("#gameButtons")
     let cardsDealt = res.data.cards
     // console.log(cardsDealt)
-
- for(let i = 0; i < cardsDealt.length; i++){
+   
+for(let i = 0; i < cardsDealt.length; i++){
+    // let currentScore = document.createElement('p')
+    //     currentScore.innerText = cardsDealt[i].value
     let playerCards = cardsDealt[i].image
      let img = document.createElement('img')
      img.src = playerCards
+    //  startGameDIV.append(currentScore)
      startGameDIV.append(img)
+     img.append(playerCards)
     // console.log(playerCards)
-    img.append(playerCards)
  }
+
+ let hitButton = document.createElement("button")
+ hitButton.innerHTML= "HIT"
+ hitButton.addEventListener("click", hitAction)
+
+ let stayButton = document.createElement("button")
+ stayButton.innerHTML= "STAY"
+ stayButton.addEventListener("click", stayAction)
+
+ gameButtonDiv.append(hitButton)
+ gameButtonDiv.append(stayButton)
+}
+
+const hitAction = async(res) => {
+    // console.log(res.data)
+    url = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`
+    let oneCardResponse = await axios.get(url)
+    
+    let hitIMG = document.querySelector("#startGame")
+    let image = oneCardResponse.data.cards
+
+    for(let i = 0; i < image.length; i++){
+        let extraCard = image[i].image
+        let newImg = document.createElement('img')
+        newImg.src = extraCard
+        console.log(extraCard)
+
+         hitIMG.append(newImg)
+         newImg.append(extraCard)
+    }
+    console.log(image)
+}
+
+const stayAction = () => {
+    console.log("hi")
 
 }
